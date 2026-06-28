ea0e6a4c63c1dcf031240612c6eb83959c8c3404
Zenith Web Dev <okikiisrael1@gmail.com>
2026-06-28 01:43:22 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
+  "https://unihelp-testing.vercel.app",
+  "https://unihelp-testing.vercel.app/",
 ].filter(Boolean);
 
 const corsOptions = {
